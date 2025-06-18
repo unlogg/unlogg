a74c22768e491076011ce0ec21458faba2ffe53a
mnove <novelli3000@gmail.com>
2025-06-18 17:06:19 +0200

    feat: add shadcn ui package

---
+++ packages/ui/src/components/unlogg-ui/test.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@unlogg/ui/components/button";
+
+const Hello = () => {
+  return (
+    <div className="bg-blue-800">
+      Helloss
+      <Button>Hello</Button>
+    </div>
+  );
+};
+
+export default Hello;
