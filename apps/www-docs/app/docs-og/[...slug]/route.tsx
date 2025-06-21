00842f67b6fb4b21f8d0c5489d8cca522e7f2dab
mnove <novelli3000@gmail.com>
2025-06-21 09:48:10 +0200

    fix: og fonts

--- apps/www-docs/app/docs-og/[...slug]/route.tsx
+++ apps/www-docs/app/docs-og/[...slug]/route.tsx
@@ -5,8 +5,21 @@ import { Icons } from "@/components/icons";
 import { generateOGImage } from "./og";
 import { readFileSync } from "node:fs";
 
-const font = readFileSync("./app/docs-og/[...slug]/DMSans-regular.ttf");
-const fontBold = readFileSync("./app/docs-og/[...slug]/Sora-Bold.ttf");
+// const font = readFileSync("./app/docs-og/[...slug]/DMSans-regular.ttf");
+// const fontBold = readFileSync("./app/docs-og/[...slug]/Sora-Bold.ttf");
+
+const dmRegularFont = readFileSync("./public/fonts/DMSans-regular.ttf");
+const soraBoldFont = readFileSync("./public/fonts/Sora-Bold.ttf");
+
+// Make sure the font exists in the specified path:
+// const dmRegularFontP = fetch(
+//   new URL("../../../public/fonts/DMSans-Regular.ttf", import.meta.url)
+// ).then((res) => res.arrayBuffer());
+
+// const soraBoldFontP = fetch(
+//   new URL("../../../public/fonts/Sora-Bold.ttf", import.meta.url)
+// ).then((res) => res.arrayBuffer());
+
 export async function GET(
   _req: Request,
   { params }: { params: Promise<{ slug: string[] }> }
@@ -15,28 +28,41 @@ export async function GET(
   const page = source.getPage(slug.slice(0, -1));
   if (!page) notFound();
 
-  return generateOGImage({
-    title: page.data.title,
-    description: page.data.description,
-    fonts: [
-      {
-        name: "Sora",
-        data: fontBold,
-        weight: 600,
-        style: "normal",
-      },
-      {
-        name: "DM Sans",
-        data: font,
-        weight: 400,
-        style: "normal",
-      },
-    ],
+  try {
+    // const [dmRegularFont, soraBoldFont] = await Promise.all([
+    //   dmRegularFontP,
+    //   soraBoldFontP,
+    // ]);
 
-    // site: "unlogg",
-    // primaryColor: "#353532",
-    // primaryTextColor: "#b9ee0b",
-  });
+    return generateOGImage({
+      title: page.data.title,
+      description: page.data.description,
+      fonts: [
+        {
+          name: "Sora",
+          // data: fontBold,
+          data: soraBoldFont,
+          weight: 600,
+          style: "normal",
+        },
+        {
+          name: "DM Sans",
+          data: dmRegularFont,
+          weight: 400,
+          style: "normal",
+        },
+      ],
+
+      // site: "unlogg",
+      // primaryColor: "#353532",
+      // primaryTextColor: "#b9ee0b",
+    });
+  } catch (error) {
+    console.error(`${error}`);
+    return new Response(`Failed to generate the image`, {
+      status: 500,
+    });
+  }
 }
 
 export function generateStaticParams() {
